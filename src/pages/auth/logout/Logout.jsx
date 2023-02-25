1749ae719d51f81d9af421767e3f3ad310051793
hatem782 <hatembenechikh100@gmail.com>
2023-02-25 23:18:31 +0100

    adding icons and logout

---
+++ src/pages/auth/logout/Logout.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+
+function Logout() {
+  useEffect(() => {
+    // here just delete the global user
+  }, []);
+  return <div></div>;
+}
+
+export default Logout;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,19 @@
+// ------------------------- COMPONENTS --------------------------------
 import Register from "../pages/auth/register/Register";
 import Login from "../pages/auth/login/Login";
+import Logout from "../pages/auth/logout/Logout";
 import Dashboard from "../pages/dashboard/Dashboard";
 
 import ManageStudents from "../pages/dashboard/ManageStudents/ManageStudents";
 import ManageTeachers from "../pages/dashboard/ManageTeachers/ManageTeachers";
 import Profile from "../pages/dashboard/Profile/Profile";
 
+// ------------------------- ICONS --------------------------------
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
+import SchoolIcon from "@mui/icons-material/School";
+import CoPresentIcon from "@mui/icons-material/CoPresent";
+// ------------------------- ROLES --------------------------------
 import Roles from "../custom/roles";
 
 const { ADMIN, ALL, ALUMINIE, RESPONSIBLE, STUDENT, SUPERADMIN, TEACHER } =
@@ -25,18 +33,29 @@ const logged = {
 };
 
 const dashboard = {
-  routes: [{ path: "/profile", Component: Profile, role: [ALL] }],
   routes: [
+    {
+      path: "/profile",
+      Component: Profile,
+      icon: <AccountBoxIcon />,
+      role: [ALL],
+    },
+    {
+      path: "/logout",
+      Component: Logout,
+      icon: <MeetingRoomIcon />,
+      role: [ALL],
+    },
     {
       path: "/gest_students",
       Component: ManageStudents,
+      icon: <SchoolIcon />,
       role: [ADMIN, SUPERADMIN, TEACHER, RESPONSIBLE],
     },
-  ],
-  routes: [
     {
       path: "/ManageTeachers",
-      Component: ManageStudents,
+      Component: ManageTeachers,
+      icon: <CoPresentIcon />,
       role: [ADMIN, SUPERADMIN],
     },
   ],
